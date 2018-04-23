6a60b2814da810ab7994685b64af692219cdef34
Mr-Pro <longer499@163.com>
2018-04-23 13:36:56 +0800

    修改会员号生成器，防止多线程生成相同id

--- src/main/java/cn/lger/util/MemberNumberRandomUtil.java
+++ src/main/java/cn/lger/util/MemberNumberRandomUtil.java
@@ -12,6 +12,11 @@ public class MemberNumberRandomUtil {
     public static SimpleDateFormat date = new SimpleDateFormat("yyyyMMddHHmmssSS");
 
     public synchronized static String randomMemberNumber(){
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return date.format(System.currentTimeMillis());
     }
 }
